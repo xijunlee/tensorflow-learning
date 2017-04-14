553bcacefdb422ee3a4e6c011a5037d91d0f16ac
xijun.lee <xijun.lee@hotmail.com>
2017-04-14 20:21:18 +0800

    modified example code

--- tensorflow-master-tensorflow-examples-tutorials/mnist/mnist_softmax.py
+++ tensorflow-master-tensorflow-examples-tutorials/mnist/mnist_softmax.py
@@ -23,7 +23,7 @@ from __future__ import division
 from __future__ import print_function
 
 # Import data
-from tensorflow.examples.tutorials.mnist import input_data
+import input_data
 
 import tensorflow as tf
 
