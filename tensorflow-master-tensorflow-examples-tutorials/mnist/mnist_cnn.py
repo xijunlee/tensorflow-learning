252bb0aa18e065d091f6f47c40788ccb4e826f2b
xijun.lee <xijun.lee@hotmail.com>
2017-04-16 14:51:19 +0800

    mnist_cnn.py

--- tensorflow-master-tensorflow-examples-tutorials/mnist/mnist_cnn.py
+++ tensorflow-master-tensorflow-examples-tutorials/mnist/mnist_cnn.py
@@ -26,6 +26,8 @@ if __name__ == '__main__':
 	sess = tf.InteractiveSession()
 	
 	# Create model
+        W = tf.Variable(tf.zeros([784,10]))
+        b = tf.Variable(tf.zeros([10]))
 	x = tf.placeholder("float", shape=[None, 784])
 	y_ = tf.placeholder("float", shape=[None, 10])
 	y = tf.nn.softmax(tf.matmul(x,W) + b)
@@ -69,12 +71,12 @@ if __name__ == '__main__':
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 	sess.run(tf.initialize_all_variables())
 	for i in range(20000):
-	  batch = mnist.train.next_batch(50)
-	  if i%100 == 0:
-	    train_accuracy = accuracy.eval(feed_dict={
+            batch = mnist.train.next_batch(50)
+	    if i%100 == 0:
+	        train_accuracy = accuracy.eval(feed_dict={
 	        x:batch[0], y_: batch[1], keep_prob: 1.0})
-    	print "step %d, training accuracy %g"%(i, train_accuracy)
-  	train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    	        print "step %d, training accuracy %g"%(i, train_accuracy)
+  	    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
 	print "test accuracy %g"%accuracy.eval(feed_dict={
     	x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
